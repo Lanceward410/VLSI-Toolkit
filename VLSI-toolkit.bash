fa7d58f04a7085a7a42f1cf3e7f09be6bdb555d3
Lance Ward <Lanceward410@gmail.com>
2025-02-20 10:47:43 -0500

    Update VLSI-toolkit.bash
    Testing out 'exec newgrp docker'

--- VLSI-toolkit.bash
+++ VLSI-toolkit.bash
@@ -1,21 +1,18 @@
 #!/bin/bash
 
-GREEN='\033[32m'
-RED='\033[31m'
-DEFAULT='\033[0m'
 set -e
-trap 'echo "${RED}Error on line $LINENO: $BASH_COMMAND ${DEFAULT}" >&2' ERR
+trap 'echo "Error on line $LINENO: $BASH_COMMAND" >&2' ERR
 
-echo "${GREEN}Updating system packages...${DEFAULT}"
+echo "Updating system packages..."
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt --fix-broken install -y
 
-echo "${GREEN}Installing dependencies...${DEFAULT}"
+echo "Installing dependencies..."
 sudo apt install -y build-essential python3 python3-venv python3-pip python3-tk \
     ca-certificates curl make git gedit mesa-utils x11-apps x11-utils bison flex \
     tcl tcl-dev tk tk-dev libxpm4 libxpm-dev xterm magic ngspice
 
-echo "${GREEN}Installing Xschem...${DEFAULT}"
+echo "Installing Xschem..."
 if [ ! -d "$HOME/xschem-src" ]; then
     git clone https://github.com/StefanSchippers/xschem.git ~/xschem-src
 fi
@@ -26,7 +23,7 @@ make -j$(nproc)
 sudo make install
 cd ~
 
-echo "${GREEN}Installing Docker for OpenLane...${DEFAULT}"
+echo "Installing Docker for OpenLane..."
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -39,12 +36,12 @@ echo \
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
-echo "${GREEN}Adding user to Docker group...${DEFAULT}"
+echo "Adding user to Docker group..."
 sudo groupadd -f docker
 sudo usermod -aG docker $USER
-newgrp docker
+exec newgrp docker
 
-echo "${GREEN}Cloning and updating OpenLane...${DEFAULT}"
+echo "Cloning and updating OpenLane..."
 if [ ! -d "$HOME/OpenLane" ]; then
     git clone https://github.com/The-OpenROAD-Project/OpenLane ~/OpenLane
 fi
