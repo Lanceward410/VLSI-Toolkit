8718c280ba615aa810de3e658d380986b78b6ed4
Lance Ward <Lanceward410@gmail.com>
2025-02-20 11:22:30 -0500

    Update toolkit2.bash

--- toolkit2.bash
+++ toolkit2.bash
@@ -3,6 +3,7 @@
 set -e
 trap 'echo "Error on line $LINENO: $BASH_COMMAND" >&2' ERR
 
+cd ~
 echo "Cloning and updating OpenLane..."
 if [ ! -d "$HOME/OpenLane" ]; then
     git clone https://github.com/The-OpenROAD-Project/OpenLane ~/OpenLane
