8826b7ce94b3f74cfa048b1ecd17e4004811a1cb
Lance Ward <Lanceward410@gmail.com>
2025-02-20 11:14:51 -0500

    Update toolkit2.bash
    Added missing component 'trap' for 'set -e'

--- toolkit2.bash
+++ toolkit2.bash
@@ -1,6 +1,8 @@
 #!bin/bash
 
 set -e
+trap 'echo "Error on line $LINENO: $BASH_COMMAND" >&2' ERR
+
 echo "Cloning and updating OpenLane..."
 if [ ! -d "$HOME/OpenLane" ]; then
     git clone https://github.com/The-OpenROAD-Project/OpenLane ~/OpenLane
